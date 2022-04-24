236bf6328f2767a25c575a4355c9114a4862a0d0
Fulmineo64 <th3nick94@gmail.com>
2022-04-24 19:02:01 +0200

    WIP migration to 1.18.2

--- src/main/java/dev/fulmineo/guild/init/ServerEventInit.java
+++ src/main/java/dev/fulmineo/guild/init/ServerEventInit.java
@@ -8,14 +8,14 @@ import dev.fulmineo.guild.Guild;
 import dev.fulmineo.guild.network.ServerNetworkManager;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.resource.ServerResourceManager;
+import net.minecraft.resource.LifecycledResourceManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayerEntity;
 
 public class ServerEventInit {
 	public static void init() {
 		ServerLifecycleEvents.END_DATA_PACK_RELOAD.register(new ServerLifecycleEvents.EndDataPackReload(){
-			public void endDataPackReload(MinecraftServer server, ServerResourceManager serverResourceManager, boolean success) {
+			public void endDataPackReload(MinecraftServer server, LifecycledResourceManager serverResourceManager, boolean success) {
 				NbtCompound nbt = ServerNetworkManager.getClientDataNbt();
 				List<ServerPlayerEntity> playerList = server.getPlayerManager().getPlayerList();
 				for (ServerPlayerEntity player: playerList) {
--- src/main/java/dev/fulmineo/guild/mixin/StructurePoolsMixin.java
+++ src/main/java/dev/fulmineo/guild/mixin/StructurePoolsMixin.java
@@ -5,35 +5,31 @@ import net.minecraft.structure.pool.StructurePoolElement;
 import net.minecraft.structure.pool.StructurePools;
 import net.minecraft.structure.processor.StructureProcessorLists;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.BuiltinRegistries;
+import com.mojang.datafixers.util.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import dev.fulmineo.guild.feature.ModifiableStructurePool;
 
 @Mixin(StructurePools.class)
 public class StructurePoolsMixin {
     @Inject(method = "register", at = @At("HEAD"), cancellable = true)
     private static void registerMixin(StructurePool spool, CallbackInfoReturnable<StructurePool> cir) {
         StructurePool pool = spool;
-        pool = tryAddElementToPool(new Identifier("village/plains/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
-        pool = tryAddElementToPool(new Identifier("village/desert/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
-        pool = tryAddElementToPool(new Identifier("village/savanna/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
-        pool = tryAddElementToPool(new Identifier("village/taiga/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
-        pool = tryAddElementToPool(new Identifier("village/snowy/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
-
-        cir.setReturnValue(BuiltinRegistries.add(BuiltinRegistries.STRUCTURE_POOL, pool.getId(), pool));
-        cir.cancel();
+       	tryAddElementToPool(new Identifier("village/plains/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
+       	tryAddElementToPool(new Identifier("village/desert/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
+       	tryAddElementToPool(new Identifier("village/savanna/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
+       	tryAddElementToPool(new Identifier("village/taiga/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
+       	tryAddElementToPool(new Identifier("village/snowy/houses"), pool, "guild:village/plains/houses/guild", StructurePool.Projection.RIGID, 2);
     }
 
-	private static StructurePool tryAddElementToPool(Identifier targetPool, StructurePool pool, String elementId, StructurePool.Projection projection, int weight) {
+	private static void tryAddElementToPool(Identifier targetPool, StructurePool pool, String elementId, StructurePool.Projection projection, int weight) {
         if(targetPool.equals(pool.getId())) {
-            ModifiableStructurePool modPool = new ModifiableStructurePool(pool);
-            modPool.addStructurePoolElement(StructurePoolElement.ofProcessedLegacySingle(elementId, StructureProcessorLists.EMPTY).apply(projection), weight);
-            return modPool.getStructurePool();
+            StructurePoolElement element = StructurePoolElement.ofProcessedLegacySingle(elementId, StructureProcessorLists.EMPTY).apply(projection);
+            for (int i = 0; i < weight; i++) {
+                ((StructurePoolAccess)pool).guild$getElements().add(element);
+            }
+            ((StructurePoolAccess)pool).guild$getElementCounts().add(Pair.of(element, weight));
         }
-        return pool;
     }
 }
